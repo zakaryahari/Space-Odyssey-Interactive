c249854fb8792c363a4a4848d8b0644d6218ccdd
zakarya hari <zakariahari42@gmail.com>
2025-10-31 17:28:42 +0100

    Ajouter des icons de Crud avec des function event pour , mais n est pas interactive .

--- js/script.js
+++ js/script.js
@@ -31,7 +31,24 @@ function renderProjects(array) {
                     <hr>
                     <p class="Missions-card-content-description">Agency: ${mission.agency} <br>
                     ${mission.objective}</p>
-                    <br> <p class="Missions-card-content-description">${mission.launchDate}</p>
+                    <hr class="mission-hr">
+                    <div class="Missions-card-status">
+                        <div class="Missions-card-status-right">
+                            <p class="Missions-card-content-description">${mission.launchDate}</p>
+                        </div>
+                        <div class="Missions-card-status-left">
+                            <button class="action-btn favorite-toggle" data-mission-id="${mission.id}">
+                                ☆  </button>
+    
+                            <button class="action-btn edit-mission-btn" data-mission-id="${mission.id}">
+                                ✎
+                            </button>
+    
+                            <button class="action-btn delete-mission-btn" data-mission-id="${mission.id}">
+                                ✖
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         `;
@@ -46,14 +63,40 @@ const show_btn = document.getElementById('add_mission_btn');
 
 // Filtering Missions by Year
 
+function Filter_fun(){
+    const filter_input = document.getElementById('year_search_bar_input').value;
+    
+    console.log(filter_input);
+}
 
-const filter_input = document.getElementById('filter_year_select').value;
+const year_search_bar = document.getElementById('year_search_bar_input');
 
-console.log(filter_input);
-
-
+year_search_bar.addEventListener('change',Filter_fun);
 // show_btn.addEventListener('click', renderProjects);
 
+// Favorite-toggle
+
+function favorite_toggle_fun() {
+    const favorite_toggle = document.querySelector('.favorite-toggle');
+
+    if (favorite_toggle.classList.contains('is-favorited')) {
+
+        favorite_toggle.textContent = '☆';
+        favorite_toggle.classList.remove('is-favorited');
+
+    }else{
+
+        favorite_toggle.textContent = '★';
+        favorite_toggle.classList.add('is-favorited');
+    }
+    favorite_toggle.classList.toggle('is-favorited');
+}
+
+
+const favorite_toggle = document.querySelector('.favorite-toggle');
+console.log(favorite_toggle);
+
+favorite_toggle.addEventListener('click',favorite_toggle_fun);
 
 
 
